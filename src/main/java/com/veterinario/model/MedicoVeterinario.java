b3a1172ff9c20bcb0f7a05d0b94c807e8c874ba5
joaquimborges <joaquim.borges1993@gmail.com>
2021-09-13 11:34:41 -0300

    ajustado

--- src/main/java/com/veterinario/model/MedicoVeterinario.java
+++ src/main/java/com/veterinario/model/MedicoVeterinario.java
@@ -1,11 +1,16 @@
 package com.veterinario.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MedicoVeterinario extends Pessoa {
 
     private Integer numeroRegistro;
     private String especialidade;
 
-    public MedicoVeterinario(String nomeDoMedico, String sobrenomeDoMedico, Integer cpf, Integer numeroRegistro, String especialidade) {
+    public MedicoVeterinario(String nomeDoMedico, String sobrenomeDoMedico, String cpf, Integer numeroRegistro, String especialidade) {
         super(nomeDoMedico, sobrenomeDoMedico, cpf);
         this.numeroRegistro = numeroRegistro;
         this.especialidade = especialidade;
